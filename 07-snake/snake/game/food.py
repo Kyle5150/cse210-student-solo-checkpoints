f4a2e93c9c93b680db44308cbcfc148bd20d9ee6
Kyle5150 <joh19128@byui.edu>
2021-10-26 11:14:06 -0600

    Update food.py

--- 07-snake/snake/game/food.py
+++ 07-snake/snake/game/food.py
@@ -3,4 +3,22 @@ from game import constants
 from game.actor import Actor
 from game.point import Point
 
+class Food(Actor):
+
+    def __init__(self):
+        super().__init__()
+        self._points = 0
+        self.set_text("@")
+        self.reset()
+
+    def get_points(self):
+        return self._points
+
+    def reset(self):
+        self._points = random.randint(1, 5)
+        x = random.randint(1, constants.MAX_X)
+        y = random.randint(1, constants.MAX_Y)
+        position = Point(x, y)
+        self.set_position(position)
+
 # TODO: Define the Food class here
